8fb61247913d9ac2d08eae09b94a45d742bdb145
Eric Nelson <ernelson12o0@gmail.com>
2023-04-12 16:21:54 -0400

    load api content on to DOM

---
+++ index.js
@@ -0,0 +1,50 @@
+function displayAPIInfo() {
+    // Access the info div
+    const infoDiv = document.getElementById('info');
+  
+    // API endpoint
+    const url = 'https://rickandmortyapi.com/api/character';
+  
+    // Fetch data from the API
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // Create a container for each card
+        const container = document.createElement('div');
+        container.classList.add('container');
+  
+        // Iterate through the data and create a card for each item
+        data.results.forEach(item => {
+          const card = document.createElement('div');
+          card.classList.add('card');
+  
+          // Create an image element and set its src attribute
+          const img = document.createElement('img');
+          img.src = item.image;
+          img.alt = item.name;
+  
+          // Create a heading element and set its text content
+          const heading = document.createElement('h2');
+          heading.textContent = item.name;
+  
+          // Create a paragraph element and set its text content
+          const paragraph = document.createElement('p');
+          paragraph.textContent = item.species;
+  
+          // Append the image, heading, and paragraph elements to the card
+          card.appendChild(img);
+          card.appendChild(heading);
+          card.appendChild(paragraph);
+  
+          // Append the card to the container
+          container.appendChild(card);
+        });
+  
+        // Append the container to the info div
+        infoDiv.appendChild(container);
+      })
+      .catch(error => console.error(error));
+  }
+  
+  // Call the displayAPIInfo function on page load
+  window.addEventListener('load', displayAPIInfo);
