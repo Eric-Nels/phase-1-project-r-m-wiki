14e56ce5776fa3dda212e4b4e1769c1627b9ddb2
Eric Nelson <ernelson12o0@gmail.com>
2023-04-19 15:15:41 -0400

    add comments

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 function displayAPIInfo() {
-    // Access card container div
+    //access card container div
     const infoDiv = document.getElementById('card-container');
 
-    // API endpoint
+    //API endpoint
     const url = 'https://rickandmortyapi.com/api/character';
 
-    // Fetch data from the API
+    //fetch data from the API
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Create a container for each card
+            //create a container for each card
             const container = document.createElement('div');
             container.classList.add('container');
 
-            // Iterate through the data and create a card for each item
+            //iterate through the data and create a card for each item
             data.results.forEach(item => {
                 const card = document.createElement('div');
                 card.classList.add('card');
 
-                //Create card for img and hidden text
+                //create card for img and hidden text
                 const imgCard = document.createElement('div');
                 imgCard.classList.add('imgCard');
 
-                //Create ul for hidden text and set its text content 
+                //create ul for hidden text 
                 const hiddenText = document.createElement('ul');
                 hiddenText.classList.add('hidden-text');
+                //create li for data
                 const li1 = document.createElement('li');
                 li1.classList.add('status');
                 const li2 = document.createElement('li');
                 li2.classList.add('gender');
                 const li3 = document.createElement('li');
                 li3.classList.add('origin');
+                //set li text content
                 li1.textContent = item.status;
                 li2.textContent = item.gender;
                 li3.textContent = item.origin.name;
+                //append li to ul
                 hiddenText.appendChild(li1);
                 hiddenText.appendChild(li2);
                 hiddenText.appendChild(li3); 
         
 
-                // Create an image element and set its src attribute
+                //create an image element
                 const img = document.createElement('img');
+                //set its src and alt attribute
                 img.src = item.image;
                 img.alt = item.name;
 
-                // Create a heading element and set its text content
+                //create a heading element and set its text content
                 const heading = document.createElement('h2');
                 heading.textContent = item.name;
 
-                // Create a paragraph element and set its text content
+                //create a paragraph element and set its text content
                 const paragraph = document.createElement('p');
                 paragraph.textContent = item.species;
 
-                // Create a like button element and set its text content
+                //create a like button element and set its text content
                 const likeButton = document.createElement('button');
                 likeButton.classList.add('like-button');
                 likeButton.textContent = 'Like';
@@ -60,7 +64,7 @@ function displayAPIInfo() {
                 const heart = document.createElement('span');
                 heart.classList.add('heart-icon');
 
-                // Append the image, heading, and paragraph elements to the card
+                //append the image, heading, and paragraph elements to the card
                 imgCard.appendChild(img);
                 imgCard.appendChild(hiddenText);
                 card.appendChild(imgCard);
@@ -69,32 +73,35 @@ function displayAPIInfo() {
                 card.appendChild(likeButton);
                 card.appendChild(heart);
 
+                //toggle the filled class on the heart icon
                 likeButton.addEventListener('click', () => {
-                    heart.classList.toggle('filled'); // Toggle the filled class on the heart icon
+                    heart.classList.toggle('filled');
                   });
 
+                //add mouseover event to image
                 imgCard.addEventListener('mouseover', () => {
                     img.style.opacity = 0;
                   });
         
-        
+                //add mouseleave event to image
                 imgCard.addEventListener('mouseleave', () => {
                     img.style.opacity = 1;
                 });
 
-                // Append the card to the container
+                //append the card to the container
                 container.appendChild(card);
             });
 
-            // Append the container to the info div
+            //append the container to the info div
             infoDiv.appendChild(container);
 
             
 
-            // Add event listeners to the checkboxes
+            //access checkboxes 
             const humanCheckbox = document.getElementById('human');
             const alienCheckbox = document.getElementById('alien');
 
+            //add event listeners to the checkboxes
             humanCheckbox.addEventListener('change', () => {
                 filterCardsBySpecies('Human');
             });
@@ -105,12 +112,13 @@ function displayAPIInfo() {
         })
         .catch(error => console.error(error));
 
-
-function filterCardsBySpecies(species) {
+  //create filter function
+  function filterCardsBySpecies(species) {
+    //access cards and checkboxes
     const cards = document.querySelectorAll('.card');
     const humanCheckbox = document.getElementById('human');
     const alienCheckbox = document.getElementById('alien');
-  
+    //iterate through the data and display cards that meet the requirements 
     cards.forEach(card => {
       const speciesParagraph = card.querySelector('p');
 
